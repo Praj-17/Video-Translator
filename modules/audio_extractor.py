0984d596e352c47d1ff67ebfc3bf0315a155a0c6
prajwal <pwaykos1@gmail.com>
2024-04-18 16:46:46 -0500

    updated audio_extractor to use ffmeg instead o moviepy

--- modules/audio_extractor.py
+++ modules/audio_extractor.py
@@ -1,12 +1,14 @@
 from moviepy.editor import VideoFileClip
 from modules.file_organizer import FileOrganizer
 import os
+import ffmpeg
+
 
 class AudioExtractor():
     def __init__(self) -> None:
         self.fo = FileOrganizer()
         pass
-    def extract_audio_to_mp3(self, video_path):
+    def extract_audio_from_video(self, video_path):
         video_clip = VideoFileClip(video_path) 
 
 
@@ -24,6 +26,16 @@ class AudioExtractor():
         # Close the video and audio clips
         video_clip.close()
         audio_clip.close()
+        return output_path
+    def extract_audio_to_mp3(self, video_path):
+        output_path_folder,updated_video_path = self.fo.initialize(video_path)
+        output_path = os.path.join(output_path_folder, self.fo.get_file_name_without_extension_from_path(video_path) + ".mp3")
+
+        # Load the video file
+        input_file = ffmpeg.input(video_path)
+
+        # Extract the audio and save it as an MP3 file
+        input_file.output(output_path, acodec='mp3').overwrite_output().run()
         return output_path
 
 if __name__ == "__main__":
